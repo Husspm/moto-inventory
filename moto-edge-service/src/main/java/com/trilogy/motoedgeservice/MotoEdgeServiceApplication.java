cff1f3d1a792e00a24a2aad20a15246e03f997ef
Patrick Hussey <husspm@gmail.com>
2019-10-25 09:49:02 -0400

    Edge Service Skeleton up

---
+++ moto-edge-service/src/main/java/com/trilogy/motoedgeservice/MotoEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trilogy.motoedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoEdgeServiceApplication.class, args);
+	}
+
+}
